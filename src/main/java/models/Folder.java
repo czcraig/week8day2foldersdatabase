14a8b298ad24df39ad71cce04e99eed2193a7f72
Chris Craig <ctc2@live.co.uk>
2018-07-10 15:47:45 +0100

    folder set up

--- src/main/java/models/Folder.java
+++ src/main/java/models/Folder.java
@@ -1,4 +1,36 @@
 package models;
 
+import java.util.List;
+
 public class Folder {
+
+    private int id;
+    private String title;
+    private List<File> files;
+
+    public Folder(){}
+
+    public Folder(String title){
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
 }
